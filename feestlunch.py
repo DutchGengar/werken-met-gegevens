402960dad1c5ca9241eb2969cc8fe0362feb2313
Dutch Gliscor <99057167@mydavinci.nl>
2021-09-08 13:22:31 +0200

    Feest en speeldag

---
+++ feestlunch.py
@@ -0,0 +1,9 @@
+aantalcroissants = 17
+prijscroissants = 0.39
+aantalstokbrooden = 2
+prijsstokbrooden = 2.78
+aantalkortingsbonnen = 3
+prijskortingsbonnen = 0.50
+
+print( aantalcroissants * prijscroissants * ( aantalstokbrooden * prijsstokbrooden ) -  ( aantalkortingsbonnen * prijskortingsbonnen)
+ )
---
+++ speeldag.py
@@ -0,0 +1,7 @@
+prijstoegangsticket = 7.45
+aantalvrienden = 3
+prijsgameseat = 0.37
+aantalminutenperpersoon = 9
+
+print( aantalvrienden * prijstoegangsticket - (prijsgameseat * aantalminutenperpersoon )
+)
